7e6616726d8531e5e208a6cca96b82682ba8927d
Wick Bushong <wickbushong@gmail.com>
2020-07-07 16:06:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,11 @@ end
 
 
 def take_a_number(queue, person)
-
-  puts "Welcome, #{person}. You are number #{queue.length+1} in line."
-
+  if queue.length == 0
+    puts "The line is currently empty."
+  else
+    puts "Welcome, #{person}. You are number #{queue.length+1} in line."
+  end
   queue << person
 end
 
