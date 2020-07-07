c7c7aac40dcf3fb5aa11fdf6544e6d771c713258
Wick Bushong <wickbushong@gmail.com>
2020-07-07 16:17:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def now_serving(queue)
   if queue.length == 0
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{queue[0]}."
     queue = queue.shift()
